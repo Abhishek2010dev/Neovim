965af643f925f6ed2bf4e4a32a7cd5bf61e67188
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-28 17:31:42 +0530

    treesitter and mason updated

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,52 @@ return {
 	{ "neovim/nvim-lspconfig" },
 	{ "hrsh7th/cmp-nvim-lsp" },
 	{ "hrsh7th/nvim-cmp" },
-	{ "williamboman/mason.nvim" },
-	{ "nvim-treesitter/nvim-treesitter" },
-	{ "williamboman/mason-lspconfig.nvim" },
+
+	{
+		"williamboman/mason.nvim",
+		dependencies = { "williamboman/mason-lspconfig.nvim" },
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"rust_analyzer",
+					"lua_ls",
+					"tsserver",
+					"gopls",
+					"html",
+					"cssls",
+					"tailwindcss",
+					"htmx-lsp",
+					"templ",
+					"prettier",
+					"stylua",
+				},
+			})
+		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = "BufReadPost",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"lua",
+					"rust",
+					"typescript",
+					"tsx",
+					"json",
+					"yaml",
+					"toml",
+					"go",
+					"html",
+					"css",
+					"java",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
