d46a428fa85244e0fc62a934275cc069e0cc97fc
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-29 12:04:38 +0530

    ibl plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,11 @@
 return {
 	'tpope/vim-sleuth',
-	{ 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }
+	{ 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		config = function()
+			require("ibl").setup()
+		end
+	}
 }
