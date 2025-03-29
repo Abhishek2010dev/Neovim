d838b70618efcd96f53539054178f9f76174df0f
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-29 11:12:55 +0530

    diagnostic complete setup

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -7,15 +7,33 @@ lspconfig_defaults.capabilities =
     vim.tbl_deep_extend("force", lspconfig_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
 
 -- Configure diagnostics to show inline errors
-vim.diagnostic.config({
-	virtual_text = { spacing = 2, prefix = "●" }, -- Enable inline errors
-	signs = true, -- Show signs in signcolumn
-	underline = true, -- Underline errors
-	update_in_insert = false, -- Don't update while typing
-	severity_sort = true, -- Sort by severity
-})
+vim.diagnostic.config {
+	severity_sort = true,
+	float = { border = 'rounded', source = 'if_many' },
+	underline = { severity = vim.diagnostic.severity.ERROR },
+	signs = vim.g.have_nerd_font and {
+		text = {
+			[vim.diagnostic.severity.ERROR] = '󰅚 ',
+			[vim.diagnostic.severity.WARN] = '󰀪 ',
+			[vim.diagnostic.severity.INFO] = '󰋽 ',
+			[vim.diagnostic.severity.HINT] = '󰌶 ',
+		},
+	} or {},
+	virtual_text = {
+		source = 'if_many',
+		spacing = 2,
+		format = function(diagnostic)
+			local diagnostic_message = {
+				[vim.diagnostic.severity.ERROR] = diagnostic.message,
+				[vim.diagnostic.severity.WARN] = diagnostic.message,
+				[vim.diagnostic.severity.INFO] = diagnostic.message,
+				[vim.diagnostic.severity.HINT] = diagnostic.message,
+			}
+			return diagnostic_message[diagnostic.severity]
+		end,
+	},
+} -- LSP keybindings
 
--- LSP keybindings
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(event)
 		local opts = { buffer = event.buf }
