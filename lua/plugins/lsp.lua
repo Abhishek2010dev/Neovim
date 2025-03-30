ead552cc26f09640a3ee78a0837cd5a699a4ab61
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-30 12:19:06 +0530

    nvim-dap setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,8 +11,8 @@ return {
 			"rafamadriz/friendly-snippets",
 		},
 	},
-	'hrsh7th/cmp-path',
-	'hrsh7th/cmp-nvim-lsp-signature-help',
+	{ 'hrsh7th/cmp-path' },
+	{ 'hrsh7th/cmp-nvim-lsp-signature-help' },
 	{
 		"williamboman/mason.nvim",
 		dependencies = { "williamboman/mason-lspconfig.nvim" },
@@ -37,8 +37,8 @@ return {
 			})
 		end,
 	},
-	"WhoIsSethDaniel/mason-tool-installer.nvim",
-	"hrsh7th/cmp-nvim-lsp",
+	{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
+	{ "hrsh7th/cmp-nvim-lsp" },
 	{
 		"folke/lazydev.nvim",
 		ft = "lua",
@@ -48,4 +48,29 @@ return {
 			},
 		},
 	},
-}
+	{
+		'mfussenegger/nvim-dap',
+		config = function()
+			local dap, dapui = require("dap"), require("dapui")
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+		end,
+	},
+
+	{
+		'rcarriga/nvim-dap-ui',
+		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+		config = function()
+			require("dapui").setup()
+		end,
+	}, }
