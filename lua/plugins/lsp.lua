844fcf25f910bb8210950e6e1ce17eb141c18f7c
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-30 12:09:07 +0530

    treesitter ensure field removed

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -31,22 +31,6 @@ return {
 		event = "BufReadPost",
 		config = function()
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"lua",
-					"rust",
-					"typescript",
-					"tsx",
-					"json",
-					"yaml",
-					"toml",
-					"html",
-					"css",
-					"java",
-					"go",
-					"gomod",
-					"gowork",
-					"gosum",
-				},
 				auto_install = true,
 				highlight = { enable = true },
 				indent = { enable = true },
@@ -64,25 +48,4 @@ return {
 			},
 		},
 	},
-	{
-		"windwp/nvim-ts-autotag",
-		config = function()
-			require('nvim-ts-autotag').setup({
-				opts = {
-					-- Defaults
-					enable_close = true, -- Auto close tags
-					enable_rename = true, -- Auto rename pairs of tags
-					enable_close_on_slash = false -- Auto close on trailing </
-				},
-				-- Also override individual filetype configs, these take priority.
-				-- Empty by default, useful if one of the "opts" global settings
-				-- doesn't work well in a specific filetype
-				per_filetype = {
-					["html"] = {
-						enable_close = false
-					}
-				}
-			})
-		end
-	}
 }
